9a5ffe11fcdcf13e3a1103b1e0d89a6b626e0ca4
Jecelyn <jecelyn.yeen@randstad.com.my>
2018-04-14 11:49:52 +0800

    commit

---
+++ pdf.js
@@ -0,0 +1,8 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://github.com/');
+  await page.pdf({ path: 'assets/github.pdf', format: 'A4' });
+  browser.close();
+})();
