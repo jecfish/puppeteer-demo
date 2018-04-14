c7e7513233da2004fccc13f020f4960d896f318e
Jecelyn <jecelyn.yeen@randstad.com.my>
2018-04-14 12:19:41 +0800

    add tracing

---
+++ tracing.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer');
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.tracing.start({ path: 'assets/trace.json' });
+  await page.goto('https://webpack.js.org/', {
+    waitUntil: 'networkidle2'
+  });
+  await page.tracing.stop();
+  browser.close();
+})();
